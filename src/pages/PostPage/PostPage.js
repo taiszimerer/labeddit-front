588b0e9d92b6cd3ad16b0ea9289bffc996590bbf
taiszimerer <taiszimerer@gmail.com>
2023-03-16 11:13:58 -0300

    feat: implementação do Hook useparams na pagina de post

--- src/pages/PostPage/PostPage.js
+++ src/pages/PostPage/PostPage.js
@@ -1,5 +1,12 @@
+import { useParams } from "react-router-dom"
+
 export const PostPage = () => {
+    const params = useParams()
+
     return (
-        <h1>Post</h1>
+        <div>
+            <h1>Post</h1>
+            {params.id}
+        </div>
     )
 }
