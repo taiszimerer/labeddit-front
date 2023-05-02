a1209a1c19b22bf30fec1ef26d9ad7129d33c7e9
taiszimerer <taiszimerer@gmail.com>
2023-05-02 14:00:30 -0300

    feat: implementação logica renderização de post

--- src/pages/FeedPage/FeedPage.js
+++ src/pages/FeedPage/FeedPage.js
@@ -4,7 +4,10 @@ import {
     Text,
     Stack,
     Image,
-    Link
+    Link,
+    FormControl,
+    Input,
+    Button
 } from '@chakra-ui/react';
 import group from "../../icons/group.png"
 import setaparacima from "../../icons/setaparacima.png"
@@ -14,15 +17,50 @@ import { useNavigate } from 'react-router-dom';
 import { goToLoginPage, goToPostPage } from '../../routes/coordinator';
 import Header from '../../components/Header';
 import { useEffect, useState } from 'react';
-import PostForm from '../../components/PostForm';
+import { BASE_URL } from "../../constants/BASE_URL"
+import axios from 'axios';
 
-export const FeedPage = (props) => {
+export const FeedPage = () => {
     const navigate = useNavigate()
-
+    const [text, setText] = useState('') //amrazena texto digitado no post
     let [posts, setPosts] = useState([]) //armazena pagina de posts (inicialmente vazia)
 
-    let handlePost = (text) => {
-        setPosts = ([...posts, text])
+    const onChangeText = (event) => {  //função atualiza o post ao digitar
+        console.log(event.target.value)
+        setText(event.target.value)
+    }
+
+    const getPosts = async () => {
+        try {
+            const config = {
+                headers: {
+                    Authorization: window.localStorage.getItem("token-labeddit")
+                }
+            }
+            const response = await axios.get(`${BASE_URL}/posts`, config)
+            setPosts(response.data)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleSubmit = async () => {
+        try {
+            const body = {
+                content: text
+            }
+
+            const response = await axios.post(
+                `${BASE_URL}/posts`, body
+            )
+
+            window.localStorage.setItem("token-labeddit", response.data.token)
+
+        } catch (error) {
+            console.log(error)
+            window.alert("Erro ao realizar post")
+        }
     }
 
     const logout = () => {
@@ -30,12 +68,18 @@ export const FeedPage = (props) => {
         goToLoginPage(navigate)
     }
 
-    // useEffect(() => {       //função que só aceita entrada no feed com login realizado!
-    //     const token = window.localStorage.getItem("token-labeddit")
-    //     if(!token){
-    //         goToLoginPage(navigate)
-    //     }
-    // }, [])
+    useEffect(() => {       //função que só aceita entrada no feed com login realizado!
+        const token = window.localStorage.getItem("token-labeddit")
+        if (!token) {
+            goToLoginPage(navigate)
+        } 
+    }, [])
+
+    useEffect(() => {   
+       getPosts()
+    }, [])
+
+    console.log(posts)
 
     return (
         <Flex
@@ -70,25 +114,39 @@ export const FeedPage = (props) => {
                         </Stack>
                     </Stack>
 
-                    <PostForm onPost={handlePost} />
-                    <Text align={'center'} color={'#FF6489'}> _________________________________________ </Text>
+                    <form onSubmit={handleSubmit}>
+                        <Box spacing={2} margin={'2px'} borderRadius={'12px'} bg={'#EDEDED'} height={'131px'} marginTop={'30px'} >
+                            <FormControl id="new-post" >
+                                <Input type="content" value={text} onChange={onChangeText} name="content" placeholder='Escreva seu post...' autoComplete='off' fontSize={'18px'} />
+                            </FormControl>
+                        </Box>
 
-                    {posts.map((post, index) => (
-                        <Box key={index} onClick={() => goToPostPage(navigate)} spacing={2} margin={'2px'} marginTop={'26px'} bg={'#FBFBFB'} borderRadius={'12px'} border={'1px solid #E0E0E0'} maxH={'200px'}>
-                            <Text color={'#6F6F6F'} fontFamily={'IBM Plex Sans'} fontWeight={'400'} fontSize={'12px'} margin={'9px'}> Enviado por: margarida03</Text>
+                        <Stack spacing={2} margin={'2px'} marginTop={'12px'}>
+                            <Button type="submit" bg={'#ff7141'} color={'white'} borderRadius={'12px'} fontSize={'18px'}
+                                >
+                                Postar
+                            </Button>
+                        </Stack>
+                    </form>
+                    <Text align={'center'} color={'#FF6489'}> ____________________________________________ </Text>
 
-                            <Text color={'#000000'} fontSize={'18px'} margin={'9px'} lineHeight={'23.4px'} cursor={'pointer'}>{post}</Text>
+                    {console.log(posts)}
+                    {posts && posts.map((post, index) => (
+                        <Box onClick={() => goToPostPage(navigate)} key={index} spacing={2} margin={'2px'} marginTop={'26px'} bg={'#FBFBFB'} borderRadius={'12px'} border={'1px solid #E0E0E0'} maxH={'200px'}>
+                            <Text color={'#6F6F6F'} fontFamily={'IBM Plex Sans'} fontWeight={'400'} fontSize={'12px'} margin={'9px'}> Enviado por: {post.creator_id} </Text>
+
+                            <Text color={'#000000'} fontSize={'18px'} margin={'9px'} lineHeight={'23.4px'} cursor={'pointer'}>{post.content}</Text>
 
                             <Stack flexDirection={'row'} gap={'5px'} margin={'10px 2px'}>
                                 <Stack padding={'1.5'} margin={'2'} borderRadius={'12px'} border={'1px solid #E0E0E0'} width={'100px'} height={'35px'} align={'center'} gap={'10px'} flexDirection={'row'} >
                                     <Image src={setaparacima} width={'18px'} cursor={'pointer'} />
-                                    <Text fontSize={'9.8px'} color={'#6F6F6F'} >1.2K</Text>
+                                    <Text fontSize={'9.8px'} color={'#6F6F6F'} >{post.likes}</Text>
                                     <Image src={setaparabaixo} width={'18px'} cursor={'pointer'} />
                                 </Stack>
 
                                 <Stack padding={'1.5'} margin={'2'} borderRadius={'12px'} border={'1px solid #E0E0E0'} width={'100px'} height={'35px'} flexDirection={'row'} align={'center'} gap={'10px'} >
                                     <Image src={coment} width={'18px'} marginLeft={'15px'} cursor={'pointer'} />
-                                    <Text fontSize={'9.8px'} color={'#6F6F6F'}>1.2K</Text>
+                                    <Text fontSize={'9.8px'} color={'#6F6F6F'}>{post.comments}</Text>
                                 </Stack>
                             </Stack>
                         </Box>
